45c09c44d1b85ff2abbc0effec2e31ab9a816783
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-04-06 13:05:59 +0900

    Removed file that shouldn't be tracked

---
+++ Firebase+RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase+RxSwift
+//
+//  Created by Hyesung Jeon on 2023/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
